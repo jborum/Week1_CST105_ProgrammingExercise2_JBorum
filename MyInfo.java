1ef309221563be1b2cde3420a9562877c17623dd
Jason Borum <33642393+jborum@users.noreply.github.com>
2017-11-30 17:42:44 -0700

    Add files via upload

---
+++ MyInfo.java
@@ -0,0 +1,27 @@
+/**
+ * This is my own work
+ * Jason E. Borum
+ * 11/24/2017
+ * Programming Exercise 1.2
+ * CST-105
+ */
+
+public class MyInfo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Print name to screen
+		System.out.println("My Name is Jason Borum.");
+		
+		//Print favorite color to screen
+		System.out.println("My Favorite Color is any color not pink or purple.");
+		
+		//Print lottery winnings to the screen
+		System.out.println("If I won the lottery, I would become a philanthropist and high school math teacher.");
+	}
+
+}
